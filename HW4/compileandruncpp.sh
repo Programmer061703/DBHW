fd861fc4f587bb9ce861035840cea1a750001457
Blake Williams <blaker1703@gmail.com>
2024-04-06 21:45:02 -0500

    Fixed: Not compiling

--- HW4/compileandruncpp.sh
+++ HW4/compileandruncpp.sh
@@ -5,7 +5,7 @@ set -e -v
 # ./hw3_soln.sh
 
 echo "Compiling..."
- g++ -Wall -I /usr/include/cppconn odbc_example.cpp -L /usr/lib -l mysqlcppconn -o hw4.exe
+ g++ -Wall -I /usr/include/cppconn hw4.cpp -L /usr/lib -l mysqlcppconn -o hw4.exe
 
 echo "Running..."
 ./hw4.exe
